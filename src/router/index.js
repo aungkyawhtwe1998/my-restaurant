aa2ef7cd3e0ed5e6644d6411108126b748ad7406
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2021-12-29 21:57:22 +0630

    initinal step

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,34 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
-
+import About from "../views/About";
+import Register from "@/views/auth/Register";
+import Login from  "@/views/auth/Login";
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
   },
+
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
+  }
 ];
 
 const router = createRouter({
+  mode:'history',
   history: createWebHashHistory(),
   routes,
 });
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row my-5">
+                <h1>Login</h1>
+                <div class="col-12 col-lg-6 m-auto">
+                    <div class="card rounded shadow-sm">
+                        <div class="card-body">
+                            <form>
+                                <div class="d-flex flex-column flex-lg-row">
+                                    <span>Enter your email</span>
+                                    <input type="text" class="form-control" name="email">
+                                </div>
+                                <div class="d-flex mt-2 flex-column flex-lg-row">
+                                    <span>Enter your password</span>
+                                    <input type="password" class="form-control" name="password">
+                                </div>
+                                <div class="d-flex mt-2 flex-column flex-lg-row">
+                                    <span>Enter your email</span>
+                                    <input type="email" class="form-control" name="email">
+                                </div>
+                                <button class="btn btn-primary mt-2" type="submit">Login</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Login"
+    }
+</script>
